de3ff4969e295bebd9face203a14bace6f8b9a4f
unknown <mel.zuercher@gmail.com>
2025-03-06 10:06:48 +0100

    showing table to added tasks

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -9,8 +9,8 @@ tasks_bp = Blueprint("tasks", __name__)
 @tasks_bp.route("/taskmanager/")
 def taskmanager():
     if "df_tasks" in session: 
-        return render_template("taskmanager.html",df_tasks=session["df_tasks"], is_empty=session["df_tasks"].empty)
-    return render_template("taskmanager.html",df_tasks=pd.DataFrame(), is_empty=True)
+        return render_template("taskmanager.html",html_table=session["df_tasks"].to_html(classes='dataframe', border=1, index=False), is_empty=session["df_tasks"].empty)
+    return render_template("taskmanager.html",html_table=None, is_empty=True)
 
 @tasks_bp.route("/add_task/", methods=["GET", "POST"])
 def add_task():
@@ -22,5 +22,5 @@ def add_task():
             session["df_tasks"]=df_new_task
         else:
             session["df_tasks"] = pd.concat([session["df_tasks"], df_new_task], ignore_index=True)
-        return render_template("taskmanager.html",df_tasks=session["df_tasks"])
+        return render_template("taskmanager.html",html_table=session["df_tasks"].to_html(index=False), is_empty=False)
     return render_template("add_task.html")
