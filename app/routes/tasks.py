c7f3b99b6cbac648b83eb1c992748cff95cdf8cf
unknown <mel.zuercher@gmail.com>
2025-03-06 15:58:03 +0100

    update populating HTML table with dictionary instead of data frame

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -8,19 +8,20 @@ tasks_bp = Blueprint("tasks", __name__)
 
 @tasks_bp.route("/taskmanager/")
 def taskmanager():
-    if "df_tasks" in session: 
-        return render_template("taskmanager.html",html_table=session["df_tasks"].to_html(classes='dataframe', border=1, index=False), is_empty=session["df_tasks"].empty)
-    return render_template("taskmanager.html",html_table=None, is_empty=True)
+    if "dict_tasks" not in session:
+        session["dict_tasks"]={}
+    return render_template("taskmanager.html",dict_tasks=session["dict_tasks"])
 
 @tasks_bp.route("/add_task/", methods=["GET", "POST"])
 def add_task():
     if request.method=="POST": 
-        df_new_task = pd.DataFrame([{"tasks": request.form.get("task"), 
-                                      "deadline": request.form.get("deadline"), 
-                                      "priority": request.form.get("priority")}])
-        if "df_tasks" not in session:
-            session["df_tasks"]=df_new_task
+        new_task = {"Task": [request.form.get("task")], 
+                                      "Deadline": [request.form.get("deadline")], 
+                                      "Priority": [request.form.get("priority")]}
+        dict_tasks=session["dict_tasks"]
+        if session["dict_tasks"]:
+            session["dict_tasks"] = {key: dict_tasks.get(key) + new_task.get(key) for key in set(dict_tasks) | set(new_task)}
         else:
-            session["df_tasks"] = pd.concat([session["df_tasks"], df_new_task], ignore_index=True)
-        return render_template("taskmanager.html",html_table=session["df_tasks"].to_html(index=False), is_empty=False)
+            session["dict_tasks"]=new_task
+        return render_template("taskmanager.html",dict_tasks=session["dict_tasks"])
     return render_template("add_task.html")
